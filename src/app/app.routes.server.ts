c666fd472311a52301c4c59df255c9f77a6504f6
Jorge Romero <jorgeromga@gmail.com>
2026-06-23 16:47:31 +0200

    fix: cambiar renderMode de pokemons/:id a Server para evitar error de prerendering

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,12 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
-   export const serverRoutes: ServerRoute[] = [
-     {
-       path: '**',
-       renderMode: RenderMode.Prerender,
-     },
-   ];
+export const serverRoutes: ServerRoute[] = [
+  {
+    path: 'pokemons/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: '**',
+    renderMode: RenderMode.Prerender,
+  },
+];
